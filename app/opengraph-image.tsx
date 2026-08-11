1dfbd0aaafdc290ec42b77a0d5d335fe45fd6678
Johnny <44646589+Junnyyy@users.noreply.github.com>
2026-08-10 23:02:07 -0500

    style(brand): align the social preview

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -2,7 +2,8 @@ import { readFile } from "node:fs/promises";
 import { join } from "node:path";
 import { ImageResponse } from "next/og";
 
-export const alt = "Subway in Motion with a blue A train line";
+export const alt =
+  "Subway in Motion with a blue A train line on a light background";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 
@@ -15,26 +16,39 @@ export default function OpenGraphImage() {
     (
       <div
         style={{
-          background: "#060709",
-          color: "#f1f3f4",
+          background: "#f7f7f7",
+          color: "#0f0e13",
           display: "flex",
           flexDirection: "column",
           fontFamily: "Geist",
           height: "100%",
-          justifyContent: "center",
-          padding: "66px 76px 82px",
+          padding: "78px 76px 82px",
           position: "relative",
           width: "100%",
         }}
       >
         <div
           style={{
+            color: "#65656a",
+            display: "flex",
+            fontSize: 19,
+            fontWeight: 400,
+            letterSpacing: "0.14em",
+            lineHeight: 1,
+          }}
+        >
+          NEW YORK CITY
+        </div>
+
+        <div
+          style={{
             display: "flex",
             flexDirection: "column",
-            fontSize: 116,
-            fontWeight: 600,
-            letterSpacing: "-0.06em",
-            lineHeight: 0.88,
+            fontSize: 98,
+            fontWeight: 400,
+            letterSpacing: "-0.035em",
+            lineHeight: 0.96,
+            marginTop: 68,
           }}
         >
           <span>Subway</span>
@@ -44,29 +58,31 @@ export default function OpenGraphImage() {
         <div
           style={{
             alignItems: "center",
-            bottom: 68,
+            bottom: 72,
             display: "flex",
             left: 0,
             position: "absolute",
           }}
         >
-          <div style={{ background: "#0039a6", height: 14, width: 914 }} />
+          <div style={{ background: "#0039a6", height: 10, width: 920 }} />
           <div
             style={{
               alignItems: "center",
               background: "#0039a6",
               borderRadius: "50%",
-              color: "#ffffff",
               display: "flex",
-              fontSize: 76,
-              fontWeight: 600,
-              height: 126,
+              height: 120,
               justifyContent: "center",
-              lineHeight: 1,
-              width: 126,
+              width: 120,
             }}
           >
-            A
+            <svg height="61" viewBox="0 0 46 54" width="52">
+              <path
+                d="M23 0 46 53.5H35.2l-4.8-12H15.6l-4.8 12H0L23 0Zm0 20-4.3 11.4h8.6L23 20Z"
+                fill="#ffffff"
+                fillRule="evenodd"
+              />
+            </svg>
           </div>
         </div>
       </div>
